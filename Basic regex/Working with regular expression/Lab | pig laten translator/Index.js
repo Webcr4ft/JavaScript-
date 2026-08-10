a014bd03dc4ac15264e03a4656c47fae9018c4b0
NeonGhost <webcr4ft001@gmail.com>
2026-08-10 13:51:40 +0100

    Add Pig Latin translation function

--- Basic regex/Working with regular expression/Lab | pig laten translator/Index.js
+++ Basic regex/Working with regular expression/Lab | pig laten translator/Index.js
@@ -1,6 +1,19 @@
-function spinalCase(str) {
-  return str
-    .replace(/([a-z])([A-Z])/g, "$1-$2")
-    .replace(/[\s_]+/g, "-")
-    .toLowerCase();
+function translatePigLatin(str) {
+  const vowels = /[aeiou]/;
+
+  // If the word starts with a vowel
+  if (vowels.test(str[0])) {
+    return str + "way";
+  }
+
+  // Find the position of the first vowel
+  const vowelIndex = str.search(vowels);
+
+  // If there are no vowels
+  if (vowelIndex === -1) {
+    return str + "ay";
+  }
+
+  // Move the consonants before the first vowel to the end
+  return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
 }
